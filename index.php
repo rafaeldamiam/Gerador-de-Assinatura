9abd1a105db974add432de2befa0e13c7f71f732
Amanda Maziero <mandahmaziero1999@gmail.com>
2022-09-12 17:05:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset="UTF-8"/>
+	<meta charset="UTF-8" />
 	<title>Gerador de Assinatura</title>
 	<link rel="icon" type="image/x-icon" href="imgs/favicon.png">
 	<!-- CSS only -->
@@ -9,9 +10,10 @@
 	<!-- JavaScript Bundle with Popper -->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </head>
+
 <body>
 
-	<div class=" card text-white bg-success mb-3 rounded-0">
+	<div class="card text-white bg-success mb-3 rounded-0">
 		<div class="container card bg-light mb-3">
 			<div class="row">
 				<h1 class="text-center text-dark">Gerador de Assinatura</h1>
@@ -22,45 +24,45 @@
 			$sig = "imgs/example.png";
 
 			?>
-
-			<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-				<div class="row">
-					<div class="col-6">
+			<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+				<div class="form-row p-2 m-2">
+					<div class="form-group col-6">
 						<!-- NOME DO FUNCIONARIO -->
 						<label class="text-dark" for="name">Nome:</label>
-						<input type="text" id="name" name="name">
+						<input type="text" id="name" name="name" class="form-control">
 					</div>
-					<div class="col-6">
+					<div class="form-group col-6">
 						<!-- CARGO -->
 						<label class="text-dark" for="role">Cargo:</label>
-						<input type="text" id="role" name="role">
+						<input type="text" id="role" name="role" class="form-control">
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-5">
+				<div class="form-row p-2 m-2">
+					<div class="form-group col-5">
 						<!-- Setor -->
 						<label class="text-dark" for="sector">Setor:</lable>
-						<input list="sectors" name="sector" id="sector">
-						<datalist id="sectors">
-							<option value="Centro de Processamento de Dados"></option>
-							<option value="Financeiro"></option>
-							<option value="Compras"></option>
-							<option value="Exportação"></option>
-							<option value="Contabilidade"></option>
-						</datalist>
+							<input list="sectors" name="sector" id="sector" class="form-control">
+							<datalist id="sectors">
+								<option value="Centro de Processamento de Dados"></option>
+								<option value="Financeiro"></option>
+								<option value="Compras"></option>
+								<option value="Exportação"></option>
+								<option value="Contabilidade"></option>
+							</datalist>
 					</div>
-					<div class="col-5">
+					<div class="form-group col-5">
 						<!-- EMAIL -->
 						<label class="text-dark" for="email">E-mail:</label>
-						<input type="email" id="email" name="email">
+						<input type="email" id="email" name="email" class="form-control">
 					</div>
-					<div class="col-2">	
+					<div class="form-group col-2">
 						<!-- RAMAL -->
 						<label class="text-dark" for="branch">Ramal:</label>
-						<input type="number" id="branch" name="branch">
+						<input type="number" id="branch" name="branch" class="form-control">
 					</div>
-				<div class="row">
-					<div class="col-12">
+				</div>
+				<div class="form-row p-2 m-2 text-center">
+					<div class="form-group col-12">
 						<!-- ENVIAR -->
 						<input type="button" class="btn btn-success" type="submit" value="Criar">
 					</div>
@@ -72,23 +74,24 @@
 		<?php
 
 		//Dump and Die - Mostre e Morra
-		function dd($data){
+		function dd($data)
+		{
 			echo '<pre>';
 			die(var_dump($data));
 			echo '</pre>';
 		}
 
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		    // Coleta os dados que vem pelo input
-		    $name = $_POST['name'];
-		    $role = $_POST['role'];
-		    $sector = $_POST['sector'];
-		    $email = $_POST['email'];
-		    $branch = $_POST['branch'];
-		    if (empty($name) || empty($role) || empty($sector) || empty($email) || empty($branch)) {
-		        echo "<script> alert('Preencha todos os campos'); </script> ";
-		    } else {
-		        // Cria imagem editavel apartir da imagem empty
+			// Coleta os dados que vem pelo input
+			$name = $_POST['name'];
+			$role = $_POST['role'];
+			$sector = $_POST['sector'];
+			$email = $_POST['email'];
+			$branch = $_POST['branch'];
+			if (empty($name) || empty($role) || empty($sector) || empty($email) || empty($branch)) {
+				echo "<script> alert('Preencha todos os campos'); </script> ";
+			} else {
+				// Cria imagem editavel apartir da imagem empty
 				$img = imagecreatefrompng('imgs/empty.png');
 				// Seleciona a cor do texto
 				$txtColor = imagecolorallocate($img, 85, 102, 66);
@@ -109,13 +112,13 @@
 				imagedestroy($img);
 				//Seleciona a assinatura real
 				$sig = "imgs/assinatura.png";
-		    }
+			}
 		}
 		//Exibe a assinatura no HTML
 		echo "<img class='rounded mx-auto d-block' src='$sig' alt='Assinatura'><br><br>";
 		?>
 
-	</div>	
+	</div>
 </body>
-</html>
 
+</html>
