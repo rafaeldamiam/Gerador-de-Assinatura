7544b18684d41f61d742b80905dedccb885fb951
rafaeldamiam <rafael.damiam23@gmail.com>
2022-09-12 16:34:42 -0300

    First Update
    Back-end: Completo
    Front-end: Inicio

--- index.php
+++ index.php
@@ -4,109 +4,118 @@
 	<meta charset="UTF-8"/>
 	<title>Gerador de Assinatura</title>
 	<link rel="icon" type="image/x-icon" href="imgs/favicon.png">
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+	<!-- JavaScript Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </head>
 <body>
 
-<h1>Gerador de Assinatura</h1>
+	<div class=" card text-white bg-success mb-3 rounded-0">
+		<div class="container card bg-light mb-3">
+			<div class="row">
+				<h1 class="text-center text-dark">Gerador de Assinatura</h1>
+			</div>
 
-<?php
-//VERIFICA SE A VARIAVEL AUXILIAR ESTA SETADA
-if (!isset($aux)) {
-	$img = "imgs/example.fw.png";
-}else{
-	$img = "imgs/assinatura.png";
-}
-echo "<img src='$img' alt='Assinatura'><br><br>";
-?>
+			<?php
+			// Seleciona a assinatura exemplo
+			$sig = "imgs/example.fw.png";
 
-	<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-		<!-- NOME DO FUNCIONARIO -->
-		<label for="name">Nome:</label>
-		<input type="text" id="name" name="name"><br>
-		<!-- CARGO -->
-		<label for="role">Cargo:</label>
-		<input type="text" id="role" name="role"><br>
-		<!-- Setor -->
-		<label for="sector">Setor:</lable>
-		<input list="sectors" name="sector" id="sector">
-		<datalist id="sectors">
-			<option value="Centro de Processamento de Dados">
-			<option value="Financeiro">
-			<option value="Compras">
-			<option value="Exportação">
-			<option value="Contabilidade">
-		</datalist><br>
-		<!-- EMAIL -->
-		<label for="email">E-mail:</label>
-		<input type="email" id="email" name="email"><br>
-		<!-- RAMAL -->
-		<label for="branch">Ramal:</label>
-		<input type="number" id="branch" name="branch"><br>
-		<!-- ENVIAR -->
-		<input type="submit" value="Criar"><br>
-	</form>
+			?>
+
+			<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+				<div class="row">
+					<div class="col-6">
+						<!-- NOME DO FUNCIONARIO -->
+						<label class="text-dark" for="name">Nome:</label>
+						<input type="text" id="name" name="name">
+					</div>
+					<div class="col-6">
+						<!-- CARGO -->
+						<label class="text-dark" for="role">Cargo:</label>
+						<input type="text" id="role" name="role">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-5">
+						<!-- Setor -->
+						<label class="text-dark" for="sector">Setor:</lable>
+						<input list="sectors" name="sector" id="sector">
+						<datalist id="sectors">
+							<option value="Centro de Processamento de Dados"></option>
+							<option value="Financeiro"></option>
+							<option value="Compras"></option>
+							<option value="Exportação"></option>
+							<option value="Contabilidade"></option>
+						</datalist>
+					</div>
+					<div class="col-5">
+						<!-- EMAIL -->
+						<label class="text-dark" for="email">E-mail:</label>
+						<input type="email" id="email" name="email">
+					</div>
+					<div class="col-2">	
+						<!-- RAMAL -->
+						<label class="text-dark" for="branch">Ramal:</label>
+						<input type="number" id="branch" name="branch">
+					</div>
+				<div class="row">
+					<div class="col-12">
+						<!-- ENVIAR -->
+						<input type="button" class="btn btn-success" type="submit" value="Criar">
+					</div>
+				</div>
+			</form>
+		</div>
+
+
+		<?php
+
+		//Dump and Die - Mostre e Morra
+		function dd($data){
+			echo '<pre>';
+			die(var_dump($data));
+			echo '</pre>';
+		}
+
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		    // Coleta os dados que vem pelo input
+		    $name = $_POST['name'];
+		    $role = $_POST['role'];
+		    $sector = $_POST['sector'];
+		    $email = $_POST['email'];
+		    $branch = $_POST['branch'];
+		    if (empty($name) || empty($role) || empty($sector) || empty($email) || empty($branch)) {
+		        echo "<script> alert('Preencha todos os campos'); </script> ";
+		    } else {
+		        // Cria imagem editavel apartir da imagem empty
+				$img = imagecreatefrompng('imgs/empty.png');
+				// Seleciona a cor do texto
+				$txtColor = imagecolorallocate($img, 85, 102, 66);
+				//imagettftext(image, size, angle, x, y, color, fontfile, text)
+				// ESCREVE NOME
+				imagettftext($img, 14, 0, 133, 25, $txtColor, "arialRegular.ttf", $name);
+				// ESCREVE CARGO
+				imagettftext($img, 11, 0, 133, 41, $txtColor, "arialRegular.ttf", $role);
+				// ESCREVE SETOR
+				imagettftext($img, 11, 0, 133, 56, $txtColor, "arialRegular.ttf", $sector);
+				// ESCREVE EMAIL
+				imagettftext($img, 11, 0, 133, 71, $txtColor, "arialRegular.ttf", $email);
+				// ESCREVE RAMAL
+				imagettftext($img, 11, 0, 330, 88, $txtColor, "arialRegular.ttf", $branch);
+				// Exporta a imagem pra png
+				imagepng($img, "imgs/assinatura.png");
+				// Destroi a imagem editavel criada
+				imagedestroy($img);
+				//Seleciona a assinatura real
+				$sig = "imgs/assinatura.png";
+		    }
+		}
+		//Exibe a assinatura no HTML
+		echo "<img class='rounded mx-auto d-block' src='$sig' alt='Assinatura'><br><br>";
+		?>
+
+	</div>	
 </body>
 </html>
 
-<?php
-	//Dump and Die - Mostre e Morra
-	function dd($data){
-		echo '<pre>';
-		die(var_dump($data));
-		echo '</pre>';
-	}
-
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	    // Coleta os dados que vem pelo input
-	    $name = $_POST['name'];
-	    $role = $_POST['role'];
-	    $sector = $_POST['sector'];
-	    $email = $_POST['email'];
-	    $branch = $_POST['branch'];
-	    if (empty($name) || empty($role) || empty($sector) || empty($email) || empty($branch)) {
-	        echo "<script> alert('Preencha todos os campos'); </script> ";
-	    } else {
-	        echo "$name<br>"; 
-	        echo "$role<br>"; 
-	        echo "$sector<br>";
-	        echo "$email<br>";
-	        echo "$branch<br>";
-//-----------------------------------------------------------------------------------------------------\\
-	    	$aux = 0;
-			//Backgroud Image
-			$img = imagecreatefrompng('imgs/empty.png');
-			//Text Color
-			$txtColor = imagecolorallocate($img, 85, 102, 66);
-			//Text Font
-			$fontPath = "arial.ttf";
-
-
-
-
-			// ESCREVE NOME
-			imagestring($img, 5, 133, 14, $name, $txtColor);
-			// ESCREVE CARGO
-			imagestring($img, 4, 133, 30, $role, $txtColor);
-			// ESCREVE SETOR
-			imagestring($img, 4, 133, 44, $sector, $txtColor);
-			// ESCREVE EMAIL
-			imagestring($img, 4, 133, 58, $email, $txtColor);
-			// ESCREVE RAMAL
-			imagestring($img, 4, 306, 74, $branch, $txtColor);
-
-			//imagefttext($img, size, angle, x, y, color, fontfile, text) 
-			//imagettftext($img, 4, 0, 0, 0, $txtColor, $fontPath, "TEXTO");
-
-			// header("Content-type: image/png");
-			// imagepng($img);
-			imagepng($img, "imgs/assinatura.png");
-			imagedestroy($img);
-
-			header("Location: imgs/assinatura.png");
-
-
-
-	    }
-
-	}
-?>
